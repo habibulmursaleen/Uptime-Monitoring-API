fa4b61fd53afd797bedd900a1e802645bd5a13bc
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-01-29 18:50:07 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 // Dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environments = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
 
-// Configuration
-app.config = {
-    port: 3000,
-};
+// // Tesing File system - Write
+// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangle' }, (err) => {
+//     console.log(err);
+// });
+// // Tesing File system - Read
+// data.read('test', 'newFile', (err, result) => {
+//     console.log(err, result);
+// });
+// // Tesing File system - Write
+// data.update('test', 'newFile', { name: 'Norway', language: 'Norsk' }, (err) => {
+//     console.log(err);
+// });
+
+// // Tesing File system - Delete
+// data.delete('test', 'newFile', (err) => {
+//     console.log(err);
+// });
 
 // create Server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port Number ${app.config.port}`);
+    server.listen(environments.port, () => {
+        console.log(`listening to port Number ${environments.port}`);
     });
 };
 
