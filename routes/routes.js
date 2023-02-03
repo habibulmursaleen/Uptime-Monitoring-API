01c9ec7c53474d4428b83bf9384499a2a1acb5ab
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-02-03 01:31:37 +0100

    token

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 // Dependencies
 const { sampleHandler } = require('../handlers/routeHandlers/sampleHandlers');
 const { userHandler } = require('../handlers/routeHandlers/userHandler');
+const { tokenHandler } = require('../handlers/routeHandlers/tokenHandler');
 
 const routes = {
     // route name : which function to hit
     sample: sampleHandler,
     user: userHandler,
+    token: tokenHandler,
 };
 
 module.exports = routes;
