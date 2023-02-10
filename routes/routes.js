bb590da07cfb4e9f162a948ea2ca0084c8f92944
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-02-10 06:59:08 +0100

    coomit

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,14 @@
 const { sampleHandler } = require('../routeHandlers/sampleHandlers');
 const { userHandler } = require('../routeHandlers/userHandler');
 const { tokenHandler } = require('../routeHandlers/tokenHandler');
+const { checkHandler } = require('../routeHandlers/checkHandler');
 
 const routes = {
     // route name : which function to hit
     sample: sampleHandler,
     user: userHandler,
     token: tokenHandler,
+    check: checkHandler,
 };
 
 module.exports = routes;
